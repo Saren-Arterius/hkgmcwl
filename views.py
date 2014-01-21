ec1f2ea56601e97a58c698217af9fb80b7864837
Shaorune <ykmran@gmail.com>
2014-01-21 23:46:53 +0800

    Rename api.py to jsonapi.py

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.core.cache import cache
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import StreamingHttpResponse
+from jsonapi import *
 
 def index(request):
     #return render(request, 'stockprice/index.html', {})
