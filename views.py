8d411bd28eed3bf7a088695895d3c94de7eed451
Shaorune <ykmran@gmail.com>
2014-01-22 00:47:49 +0800

    test template

--- views.py
+++ views.py
@@ -5,6 +5,8 @@ from django.http import StreamingHttpResponse
 from hkgmcwl.jsonapi import *
 
 def index(request):
+    return render(request, 'index.html', {})
+    
+def new(request, base64string):
     conn = MinecraftJsonApi(host = '192.168.0.1', port = 44446, username = 'admin', password = 'password')
     a = conn.call("players.name.whitelist", "saren")
-    return render(request, 'index.html', {})
