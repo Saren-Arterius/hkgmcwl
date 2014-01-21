b262c69d8497fbb636abac1c8fbc0141986b5539
Shaorune <ykmran@gmail.com>
2014-01-21 23:45:15 +0800

    Added urls and template views

--- views.py
+++ views.py
@@ -1,3 +1,8 @@
+from django.core.cache import cache
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.http import StreamingHttpResponse
 
-# Create your views here.
+def index(request):
+    #return render(request, 'stockprice/index.html', {})
+    return StreamingHttpResponse("terst")
