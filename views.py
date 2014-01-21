f18be2888552d427311dfbcdef0e682deaa3cf18
Shaorune <ykmran@gmail.com>
2014-01-21 23:50:23 +0800

    Added test method

--- views.py
+++ views.py
@@ -6,4 +6,6 @@ from jsonapi import *
 
 def index(request):
     #return render(request, 'stockprice/index.html', {})
+    conn = MinecraftJsonApi(host='localhost', port=44446, username='admin', password='password')
+    conn.call("players.name.send_message", "saren", "test")
     return StreamingHttpResponse("terst")
