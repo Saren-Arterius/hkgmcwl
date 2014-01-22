cf303d30782f419fa1ef0191bfd42428fa0cccbb
Shaorune <ykmran@gmail.com>
2014-01-22 23:53:27 +0800

    update errormsg

--- methods/errormsgs.py
+++ methods/errormsgs.py
@@ -8,8 +8,8 @@ errorMsgs = {
     "7": "HKGolden user ID already exists.",
     "8": "In-game name already exists.",
     "11": "HKGolden user ID does not exist in database.",
-    "50": "You requested validation too often. Please try 15 minutes later.",
-    "100": "HKGolden is down, or blocked our access. Please try later.",
+    "50": "You requested validation too often. Please try again 15 minutes later.",
+    "100": "HKGolden is down, or blocked our access. Please try again later.",
     "101": "The validate string is wrong. Please ensure that you have copied all the string using Ctrl+A and Ctrl+C.",
     "102": "Failed communicate with minecraft server. Please find server administrator Saren on HKGolden or WTAKO forum or online map.",
 }
