00d47aa219b40f5847b4c11c0cba0015fe45ad8d
Shaorune <ykmran@gmail.com>
2014-01-23 00:44:41 +0800

    Revert "try"
    This reverts commit c52cebbb5807a185ad876d54573d6467f86cc858.

--- urls.py
+++ urls.py
@@ -5,8 +5,7 @@ from hkgmcwl import views
 urlpatterns = patterns('',
     url(r'^success/(?P<base64encoded>.+)$', views.success, name='success'),
     
-    url(r'^password/$', views.password, name='password'),
-    url(r'^password$', redirect_to, {'url': r'^password/$'}),
+    url(r'^password/?$', views.password, name='password'),
     url(r'^password/error/(?P<code>\d*)$', views.passwordError, name='passwordError'),
     url(r'^password/(?P<hkg_uid>\d+)/error/(?P<code>\d*)$', views.passwordValidateError, name='passwordValidateError'),
     url(r'^password/(?P<hkg_uid>\d+)/validate$', views.passwordValidateDo, name='passwordValidateDo'),
