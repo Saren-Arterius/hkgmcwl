e45036ce4c8700b259110fed9d02e73c07e9ca79
Shaorune <ykmran@gmail.com>
2014-01-22 15:37:48 +0800

    rename newuser to confirm

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from hkgmcwl import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^error/(?P<code>\d*)$', views.error, name='error'),
-    url(r'^newuser/$', views.newuser, name='newuser'),
+    url(r'^confirm/$', views.confirm, name='confirm'),
 )
--- views.py
+++ views.py
@@ -12,8 +12,8 @@ def error(request, code):
     context = {"error": errorMsg}
     return render(request, 'index.html', context)
     
-def newuser(request):
-    return render(request, 'newuser.html', context)
+def confirm(request):
+    return render(request, 'confirm.html', context)
     conn = MinecraftJsonApi(host = '192.168.0.1', port = 44446, username = 'admin', password = 'password')
     a = conn.call("players.name.whitelist", "saren")
     raise Exception()
