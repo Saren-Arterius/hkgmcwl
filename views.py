80d2c00098e2cb5ee7aeb812104b35c9207cef35
Shaorune <ykmran@gmail.com>
2014-01-22 20:05:36 +0800

    fixderp

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
 from hkgmcwl.views_general import *
 from hkgmcwl.views_errormsgs import *
-from hkgmcwl.views_errorwhitelist import *
-from hkgmcwl.views_errorpassword import *
+from hkgmcwl.views_whitelist import *
+from hkgmcwl.views_password import *
