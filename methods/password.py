17c969213557282ab8c350071162a8047b010a19
Shaorune <ykmran@gmail.com>
2014-01-23 00:19:56 +0800

    wtf??????

--- methods/password.py
+++ methods/password.py
@@ -15,7 +15,7 @@ import urllib.request
 def password(request):
     if request.GET:
         if not Whitelist.objects.filter(hkg_uid = request.GET["hkg_uid"]):
-            return HttpResponseRedirect("error/{0}".format(11))
+            return HttpResponseRedirect("password/error/{0}".format(11))
         else:
             return HttpResponseRedirect("{0}".format(request.GET["hkg_uid"]))
     else:
@@ -28,7 +28,7 @@ def passwordError(request, code):
 
 def passwordValidatePage(request, hkg_uid):
     if not Whitelist.objects.filter(hkg_uid = hkg_uid):
-        return HttpResponseRedirect("password/error/{0}".format(11))
+        return HttpResponseRedirect("error/{0}".format(11))
     cache.add("password_recovery_{0}".format(hkg_uid), genPassword(24))
     validateString = cache.get("password_recovery_{0}".format(hkg_uid))
     context = {"hkg_uid": hkg_uid, "validateString": validateString, "server": randint(1,14), "href": hkg_uid}
