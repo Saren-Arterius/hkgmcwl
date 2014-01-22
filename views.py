576de6c3b0fc341ea992f8a957a5267b18a5740a
Shaorune <ykmran@gmail.com>
2014-01-22 15:38:23 +0800

    fixderp

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ def error(request, code):
     return render(request, 'index.html', context)
     
 def confirm(request):
-    return render(request, 'confirm.html', context)
+    return render(request, 'confirm.html', {})
     conn = MinecraftJsonApi(host = '192.168.0.1', port = 44446, username = 'admin', password = 'password')
     a = conn.call("players.name.whitelist", "saren")
     raise Exception()
