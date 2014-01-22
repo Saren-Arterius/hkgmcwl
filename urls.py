33605062c3157e4ae6ee529daac6e62301e87ee7
Shaorune <ykmran@gmail.com>
2014-01-22 15:27:56 +0800

    update error

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from hkgmcwl import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^error/(?P<code>\d+)$', views.error, name='error'),
+    url(r'^error/(?P<code>\d*)$', views.error, name='error'),
     url(r'^newuser/$', views.newuser, name='newuser'),
 )
