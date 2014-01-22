c0ffe653c683f0ac75871ea9cbe6f14b2fdecc00
Shaorune <ykmran@gmail.com>
2014-01-22 19:30:49 +0800

    added error message

--- views.py
+++ views.py
@@ -12,6 +12,21 @@ from re import findall
 import urllib.request
 import json
 
+errorMsg = {
+    "1": "HKGolden user ID does not exist in request",
+    "2": "In-game name does not exist in request",
+    "3": "HKGolden user ID must be digits",
+    "4": "HKGolden user ID is too long",
+    "5": "In-game name's length must be in 3-20",
+    "6": "In-game name must be matching A to Z, a to z, 0 to 9, _",
+    "7": "HKGolden user ID already exists",
+    "8": "In-game already exists",
+    "50": "You requested validation too often. Please try 15 minutes later.",
+    "100": "HKGolden is down, or blocked our access. Please try later.",
+    "101": "The validate string is wrong. Please ensure that you have copied all the string using Ctrl+A and Ctrl+C.",
+    "102": "Failed communicate with minecraft server. Please find server administrator Saren on HKGolden or WTAKO forum or online map.",
+}
+
 def index(request):
     if request.GET:
         try:
@@ -25,7 +40,7 @@ def index(request):
         return render(request, 'index.html', {})
     
 def error(request, code):
-    errorMsg = "Error code {0}".format(code)
+    errorMsg = "Error code {0}: {1}".format(code, errorMsg[code])
     context = {"error": errorMsg}
     return render(request, 'index.html', context)
     
@@ -40,7 +55,7 @@ def validatePage(request, base64encoded):
     return render(request, 'validate.html', context)
 
 def validateError(request, base64encoded, code):
-    errorMsg = "Error code {0}".format(code)
+    errorMsg = "Error code {0}: {1}".format(code, errorMsg[code])
     jsonString = b64decode(base64encoded).decode()
     data = json.loads(jsonString)
     try:
