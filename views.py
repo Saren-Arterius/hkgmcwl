4183189fbc5eb01abaa5fee82e9839d7199ee9a0
Shaorune <ykmran@gmail.com>
2014-01-22 17:10:59 +0800

    fix derp

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.core.cache import cache
 from django.shortcuts import render
 from django.http import HttpResponsePermanentRedirect
 from hkgmcwl.jsonapi import *
+from hkgmcwl.models import *
 from pyquery import PyQuery as pq
 from base64 import b64encode, b64decode
 from random import randint
