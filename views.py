3bce3b1a8f0076ba17efd51826b0aaed498d8d10
Shaorune <ykmran@gmail.com>
2014-01-22 16:00:31 +0800

    again

--- views.py
+++ views.py
@@ -16,19 +16,19 @@ def error(request, code):
     return render(request, 'index.html', context)
     
 def confirmPage(request):
-    json = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
-    base64encoded = base64.b64encode(json)
+    jsonString = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
+    base64encoded = base64.b64encode(jsonString)
     context = {"hkg_uid": HttpRequest.GET["hkg_uid"], "base64encoded": base64encoded, "server": randint(1,14)}
     return render(request, 'confirm.html', context)
     
 def confirmError(request):
-    json = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
-    base64encoded = base64.b64encode(json)
+    jsonString = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
+    base64encoded = base64.b64encode(jsonString)
     context = {"hkg_uid": HttpRequest.GET["hkg_uid"], "base64encoded": base64encoded, "server": randint(1,14)}
     return render(request, 'confirm.html', context)
     
 def confirm(request):
-    json = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
-    base64encoded = base64.b64encode(json)
+    jsonString = json.dumps({"hkg_uid": HttpRequest.GET["hkg_uid"], "ig_name": HttpRequest.GET["ig_name"]})
+    base64encoded = base64.b64encode(jsonString)
     context = {"hkg_uid": HttpRequest.GET["hkg_uid"], "base64encoded": base64encoded, "server": randint(1,14)}
     return render(request, 'confirm.html', context)
