b50e1316d53323a1a8095a013f912365cb070f73
Shaorune <ykmran@gmail.com>
2014-01-22 18:00:47 +0800

    fix url

--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ from hkgmcwl import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^error/(?P<code>\d*)$', views.error, name='error'),
-    url(r'^(?P<base64encoded>.+)$', views.confirmPage, name='confirmPage'),
     url(r'^(?P<base64encoded>.+)/confirm$', views.confirmDo, name='confirmDo'),
     url(r'^(?P<base64encoded>.+)/error$', views.confirmError, name='confirmError'),
     url(r'^(?P<base64encoded>.+)/success$', views.confirmSuccess, name='confirmSucess'),
+    url(r'^(?P<base64encoded>.+)$', views.confirmPage, name='confirmPage'),
 )
